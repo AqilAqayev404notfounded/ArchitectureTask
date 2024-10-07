0e5cc3d963a3863b92410e0a1a449dfe21184c11
AgilDeveloper <157524718+AqilAqayev404notfounded@users.noreply.github.com>
2024-10-07 19:35:21 +0400

    done

---
+++ Podcast.BLL/ViewModels/TopicViewModels/TopicViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Podcast.BLL.ViewModels.EpisodeViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Podcast.BLL.ViewModels.TopicViewModels
+{
+    public class TopicViewModel : IViewModel
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? CoverUrl { get; set; }
+        public List<EpisodeViewModel>? Episodes {  get; set; }
+    }
+}
---
+++ Podcast.MVC/Areas/Admin/Controllers/TopicController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Podcast.BLL.Services.Contracts;
+using Podcast.BLL.ViewModels.TopicViewModels;
+using Podcast.DAL.DataContext;
+using Podcast.DAL.DataContext.Entities;
+
+namespace Podcast.MVC.Areas.Admin.Controllers
+{
+
+    [AutoValidateAntiforgeryToken]
+    [Area("Admin")]
+    public class TopicController : Controller
+    {
+        private readonly ITopicService _topicService;
+
+        public TopicController(ITopicService topicService)
+        {
+            _topicService = topicService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var topic =await _topicService.GetListAsync();
+            return View(topic);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create( TopicCreateViewModel model)
+        {
+            if (!ModelState.IsValid) 
+            {
+                return View(model);
+            }
+
+
+            await _topicService.CreateAsync(model);
+            return RedirectToAction("Index");
+
+        }
+
+        public IActionResult UpdateAsync() 
+        { 
+            return View() ;
+        }
+
+        [HttpPost]
+        public async Task< IActionResult> UpdateAsync(TopicUpdateViewModel model)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            await _topicService.UpdateAsync(model);
+            return RedirectToAction("Index");
+
+
+        }
+        public IActionResult Delete() 
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Delete()
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+        }
+
+
+
+
+    }
+}
